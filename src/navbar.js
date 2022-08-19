71a2557838aaf883a1fea395f28c70be7950d8ae
Skshm31 <111271949+Skshm31@users.noreply.github.com>
2022-08-19 11:20:51 +0530

    Update navbar.js

--- src/navbar.js
+++ src/navbar.js
@@ -3,19 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './design.css';
 import { Link } from "react-router-dom";
 function Navbar(){
-    const [search,searchfind]=useState("")
-    const searchfindch=(event)=>{
-        searchfind(event.target.value)
-    }
-    const showblog=(event)=>{
-        //showblogs()
-        searchfind('')
-    }
     return (
         <div className="container">
     <div className="row">
     <nav className="navbar navbar-fixed-top navbar-expand-lg navcol " id="navbar">
-        <div className="brand"><a className="navbar-brand brand" href="#">User name</a></div>
+        <div className="brand">User name</div>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -32,9 +24,7 @@ function Navbar(){
               <Link to={{pathname:`/blogs/add`}}><div className="nav-link">Add blogs</div></Link>
               </li>
               <li>
-                <label className="searchfield"> 
-              <input value={search} onChange={searchfindch} placeholder="search the blog"></input>
-              <button onClick={showblog}>Search</button></label>
+                
                 </li>  
           </ul></div></nav></div>
 </div>
